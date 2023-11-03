48141ac66818a8bc1b95192ef4eab2e8619e265c
infopcgood <112328126+infopcgood@users.noreply.github.com>
2023-11-03 06:10:48 +0000

    Change fonts

--- src/constants/asset_constants.py
+++ src/constants/asset_constants.py
@@ -1,6 +1,6 @@
 ASSET_FOLDERS = ['sounds/', 'textures/']
 FONT_FOLDERS = ['fonts/']
-FONT_SIZES_TO_LOAD = [10, 12, 20, 36, 40]
+FONT_SIZES_TO_LOAD = [10, 12, 20, 22, 36, 40]
 
 # filename extensions are limited to 4 characters
 SOUND_ASSET_FILENAME_EXTENSION = ['.mp3', '.ogg', '.wav']
